5181602b8878e26b9f5867461ebd2cf5b1b79405
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-25 14:40:04 -0600

    insertion sort final

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -4,22 +4,25 @@ class InsertionSort
   def insort(array)
     secondary=[]
     array.each {|num|
-      index = secondary.length - 1
       if secondary.count == 0
         secondary << num
       elsif num > secondary[-1]
         secondary << num
       else
-        index_swap(secondary, num, index)
+        index_swap(secondary, num)
       end
     }
     print secondary
   end
 
-  def index_swap(secondary, num, index)
+  def index_swap(secondary, num)
+    index = secondary.length - 1
     until index < 0
       if num > secondary[index]
         secondary.insert(index + 1, num)
+        break
+      elsif index == 0
+        secondary.unshift(num)
         break
       else
         index = index - 1
@@ -29,5 +32,5 @@ class InsertionSort
 end
 
 sort = InsertionSort.new
-sort.insort(["a", "b", "z", "d", "h", "p"])
-sort.insort([1, 6, 3, 7, 2, 9, 1])
+sort.insort(["a", "b", "z", "d", "h", "p", "A"])
+sort.insort([1, 6, 3, 7, 2, 9, -1])
